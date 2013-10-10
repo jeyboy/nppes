62bc6cf668aa717bb87a9714d27b879ba38e068c
Jenua Boiko <jeyboy@bigmir.net>
2013-10-10 17:25:58 +0300

    add decoded value functions

--- lib/nppes/models/np_address.rb
+++ lib/nppes/models/np_address.rb
@@ -4,8 +4,16 @@ module Nppes
 
     validates :city, presence: true
 
+    def decoded_country
+      Nppes.decode_value(:country, country)
+    end
+
+    def decoded_state
+      Nppes.decode_value(:state, state)
+    end
+
     def to_s
-      "#{address1} | #{city}, #{state}, #{country}, #{zip}, #{phone}"
+      "#{address1} | #{city}, #{decoded_state}, #{decoded_country}, #{zip}, #{phone}"
     end
   end
 end
--- lib/nppes/models/np_license.rb
+++ lib/nppes/models/np_license.rb
@@ -3,5 +3,13 @@ module Nppes
     belongs_to :np_identifier
 
     validates :license_number, presence: true
+
+    def decoded_state
+      Nppes.decode_value(:state, license_number_state_code)
+    end
+
+    def decoded_speciality
+      Nppes.decode_value(:taxonomy_code, taxonomy_code)
+    end
   end
 end
